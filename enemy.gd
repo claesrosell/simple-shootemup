906d9a01bbfd4fd4a076d1661e9973fbfd2290f3
Marcus Rosell <marcus.rosell@solme.se>
2026-01-18 22:25:09 +0100

    Added particle effects to enemy explosion

--- enemy.gd
+++ enemy.gd
@@ -30,7 +30,8 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	print("death?")
+	pass
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	queue_free()
+	print("death")
+	animation_player.play("death")
